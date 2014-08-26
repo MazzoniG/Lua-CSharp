0e680c6dc0c913a3ab57001232687f813c9d8ba0
Guillermo Mazzoni <guillermo@2build.it>
2014-08-26 10:37:18 -0600

    Creating Repo

---
+++ VentanaPrincipal.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace LP_LuaCSharp
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RichTextBox = new System.Windows.Forms.RichTextBox();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.RichTextBox);
+            this.groupBox1.Controls.Add(this.RunButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(772, 399);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lua Script";
+            // 
+            // RichTextBox
+            // 
+            this.RichTextBox.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.RichTextBox.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RichTextBox.Location = new System.Drawing.Point(40, 47);
+            this.RichTextBox.Name = "RichTextBox";
+            this.RichTextBox.Size = new System.Drawing.Size(698, 284);
+            this.RichTextBox.TabIndex = 1;
+            this.RichTextBox.Text = "";
+            this.RichTextBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(663, 347);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(75, 36);
+            this.RunButton.TabIndex = 0;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(820, 449);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "VentanaPrincipal";
+            this.Text = "LP Project";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox RichTextBox;
+        private System.Windows.Forms.Button RunButton;
+
+    }
+}
+
---
+++ VentanaPrincipal.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LuaInterface;
+using Lua511;
+using System.IO;
+using System.Timers;
+//using luanet;
+
+
+namespace LP_LuaCSharp
+{
+    public partial class VentanaPrincipal : Form
+    {
+        public Lua lua = new Lua();
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+
+        }
+
+        public void print(string s) {
+            MessageBox.Show(s);
+        }
+
+        public void radio(RadioButton rb, TextBox tb)
+        {
+           if(rb.Name ==  "Bold" && rb.Checked == true){
+               tb.Font = new Font(tb.Font, FontStyle.Bold);
+           }
+           else
+           {
+               tb.Font = new Font(tb.Font, FontStyle.Regular);
+               tb.Font = new Font(tb.Font, FontStyle.Italic);
+           }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try {
+                //Funcion que imprime 
+                VentanaPrincipal VP = new VentanaPrincipal();
+                lua.RegisterFunction("print", VP, VP.GetType().GetMethod("print"));
+                lua.RegisterFunction("radio", VP, VP.GetType().GetMethod("radio"));
+                lua.DoString(RichTextBox.Text);
+
+
+                //lua.DoString(RichTextBox.Text);
+                //string[] TextBox = RichTextBox.Text.Split(' ');
+                //double Variable = (double)lua[TextBox[0]];
+                //Console.WriteLine(Variable); 
+
+                } catch(Exception ex){
+                }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
